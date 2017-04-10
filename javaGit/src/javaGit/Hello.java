c6a37846bc42c086178e20ecfb4b84d00134353b
chis90614 <sims2favorit@naver.com>
2017-04-10 09:57:45 +0900

    세 번째 시도

--- javaGit/src/javaGit/Hello.java
+++ javaGit/src/javaGit/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!");
-		System.out.println("¾È³ç ¼¼°è!!!");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš”!!!");
 	}
 }
